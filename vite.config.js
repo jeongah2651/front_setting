83bad7b69f65a1f645e787ef44a8f89f1975dcf0
jeongah2651 <46205870+jeongah2651@users.noreply.github.com>
2024-04-13 10:38:06 +0900

    Add pwa

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
-  root: '.', // 이 부분에서 프로젝트의 루트 디렉토리를 지정합니다.
-  build: {
+    plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+    root: '.', // 이 부분에서 프로젝트의 루트 디렉토리를 지정합니다.
+    build: {
     rollupOptions: {
-      input: 'index.html' // 여기에서 진입 파일을 지정합니다.
+        input: 'index.html' // 여기에서 진입 파일을 지정합니다.
     }
-  }
+    }
 });
