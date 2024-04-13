8a14d1f629cc5b030343eae9feea94082a14a3d2
jeongah2651 <46205870+jeongah2651@users.noreply.github.com>
2024-04-13 14:08:50 +0900

    build vite.config.js html위치 지정

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
-    // root: '.', // 이 부분에서 프로젝트의 루트 디렉토리를 지정합니다.
-    // build: {
-    // rollupOptions: {
-    //     input: 'index.html' // 여기에서 진입 파일을 지정합니다.
-    // }
-    // }
+    root: '.', // 이 부분에서 프로젝트의 루트 디렉토리를 지정합니다.
+    build: {
+        rollupOptions: {
+            input: './dist/index.html' // 여기에서 진입 파일을 지정합니다.
+        }
+    }
 });
