ac681b9abeb7d001b3ab7370040963065e42ab3b
jeongah2651 <46205870+jeongah2651@users.noreply.github.com>
2024-04-13 10:15:58 +0900

    Add Vite build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  root: '.', // 이 부분에서 프로젝트의 루트 디렉토리를 지정합니다.
+  build: {
+    rollupOptions: {
+      input: 'index.html' // 여기에서 진입 파일을 지정합니다.
+    }
+  }
 });
